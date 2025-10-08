4478a47cea33c5db7171ad8fac255e0634b3118a
wotres <wotres@naver.com>
2025-10-08 18:33:07 +0900

    [CHANGE] model url

--- config/settings.py
+++ config/settings.py
@@ -5,7 +5,7 @@ class Settings:
     WEAVIATE_HOST: str = os.getenv("WEAVIATE_HOST", "localhost")
     WEAVIATE_HTTP_PORT: int = int(os.getenv("WEAVIATE_HTTP_PORT", 8080))
     WEAVIATE_GRPC_PORT: int = int(os.getenv("WEAVIATE_GRPC_PORT", 50051))
-    MODEL_SERVER_URL: str = os.getenv("MODEL_SERVER_URL", "http://localhost:8888/v1/embeddings")
+    MODEL_SERVER_URL: str = os.getenv("MODEL_SERVER_URL", "http://localhost:8888")
 
     class Config:
         env_file = ".env"
--- services/embedding_service.py
+++ services/embedding_service.py
@@ -6,9 +6,12 @@ async def get_embedding_from_model_server(query: str):
     """
     외부 모델 서버에 query를 전송해 embedding 벡터를 가져옴 (비동기)
     """
+    embedding_url = f"{settings.MODEL_SERVER_URL}/v1/embeddings"
+
     async with httpx.AsyncClient(timeout=10.0) as client:
+
         response = await client.post(
-            settings.MODEL_SERVER_URL,
+            embedding_url,
             json={
                 "model": "mock-embedding",  # mock 서버는 model 필드도 요구함
                 "input": query              # mock 서버는 'input' 필드 사용
